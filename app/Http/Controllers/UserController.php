c6e4c671b73141ee6f6053c903ed2f84ac5537c4
Alan Sanchez <alaansannchezz@gmail.com>
2022-12-15 18:52:44 -0300

    Ejercicio 4 Completo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 
 class UserController extends Controller
 {
@@ -35,8 +38,16 @@ class UserController extends Controller
     public function check_create($name, $email)
     {
         // TASK: find a user by $name and $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        // if not found, create a user with $name, $email and random password
+        $user = User::firstOrCreate(
+            [
+                'name' => $name,
+                'email' => $email
+            ],
+            [
+                'password' => Hash::make(rand())
+            ]
+        );
 
         return view('users.show', compact('user'));
     }
@@ -69,5 +80,4 @@ class UserController extends Controller
 
         return view('users.index', compact('users'));
     }
-
 }
