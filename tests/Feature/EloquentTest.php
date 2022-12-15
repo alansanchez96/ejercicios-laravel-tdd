e1069b86aa50ccb2fe81f08f9231a04f12baaf98
Alan Sanchez <alaansannchezz@gmail.com>
2022-12-15 19:22:24 -0300

    Ejercicio 7 completo

--- tests/Feature/EloquentTest.php
+++ tests/Feature/EloquentTest.php
@@ -72,15 +72,14 @@ class EloquentTest extends TestCase
 
     public function test_create_project()
     {
-        
+
         $response = $this->post('projects', ['name' => 'Some name']);
         $response->assertRedirect();
     }
 
     public function test_mass_update_projects()
     {
-        $this->withoutExceptionHandling();
-        
+
         $project = new Project();
         $project->name = 'Old name';
         $project->save();
@@ -98,6 +97,7 @@ class EloquentTest extends TestCase
 
     public function test_check_or_update_user()
     {
+        $this->withoutExceptionHandling();
         $response = $this->get('users/check_update/john/john@john.com');
         $response->assertStatus(200);
         $this->assertDatabaseHas('users', [
